ffd9f961aed6c77bf7c2737deae8727f71099a2d
bin-nmd-x <256811159+binary-nomad-x@users.noreply.github.com>
2026-01-29 00:14:44 +0500

    code formatted

--- basics/20-celsius-to-fahrenheit-conversion.cpp
+++ basics/20-celsius-to-fahrenheit-conversion.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main()
 {
-    double celsius, fahrenheit;
+    double celsius;
 
     cout << "Enter temperature in Celsius: ";
     cin >> celsius;
 
     // Use 1.8 or (9.0 / 5.0) to ensure floating-point math
-    fahrenheit = (celsius * 1.8) + 32;
+    const double fahrenheit = (celsius * 1.8) + 32;
 
     cout << celsius << "°C is equal to " << fahrenheit << "°F" << endl;
 
