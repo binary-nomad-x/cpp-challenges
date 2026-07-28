9e8bc514aa910864e1b75ef8f6bb17fabbd17591
bin-nmd-x <256811159+binary-nomad-x@users.noreply.github.com>
2026-07-29 01:00:19 +0500

    minor adjustments

--- exercises/06-container-with-most-water/solution.cpp
+++ exercises/06-container-with-most-water/solution.cpp
@@ -1,34 +1,31 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
 static int maxArea(const vector<int> &height) {
-    int left = 0, right = height.size() - 1;
+    int left = 0;
+    int right = static_cast<int>(height.size()) - 1;
     int maxWater = 0;
 
     while (left < right) {
-        const int h = min(height[left], height[right]);
-        const int w = right - left;
-        maxWater = max(maxWater, h * w);
+        maxWater = max(
+            maxWater,
+            min(height[left], height[right]) * (right - left)
+        );
 
-        if (height[left] < height[right]) {
-            left++;
+        if (height[left] <= height[right]) {
+            ++left;
         } else {
-            right--;
+            --right;
         }
-
     }
 
     return maxWater;
 }
 
 int main() {
-    const vector<int> test1 = {1, 8, 6, 2, 5, 4, 8, 3, 7};
-    cout << "Test 1: " << maxArea(test1) << " (Expected: 49)" << endl;
-
-    const vector<int> test2 = {1, 1};
-    cout << "Test 2: " << maxArea(test2) << " (Expected: 1)" << endl;
-
-    return 0;
+    cout << maxArea({1, 8, 6, 2, 5, 4, 8, 3, 7}) << '\n'; // 49
+    cout << maxArea({1, 1}) << '\n'; // 1
 }
